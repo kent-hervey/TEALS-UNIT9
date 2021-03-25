bceb7d487f6becf374b07b713c287ba3866296b8
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-03-25 10:57:49 -0500

    Added and populated Dog.java

---
+++ src/unit9TestInheritance/Dog.java
@@ -0,0 +1,39 @@
+package unit9TestInheritance;
+
+public class Dog extends Pet
+{	
+	private String breed;
+	
+	// Creates a dog with the given name and breed
+	public Dog(String n, String s, String b) {
+	    super(n, s);
+	    this.breed = b;
+
+
+	}
+	
+	// Returns this dog's breed
+	public String getBreed()
+	{
+	    return this.breed;
+
+	}
+	
+	// Returns a string representation of this dog. The string should have the 
+            // following format:  <dog’s name> " is a "  <species> " of breed " <breed> 
+	public String toString()
+	{
+	    return this.getName() + " is a "  + this.getSpecies() + " of breed " + this.breed;
+
+
+	}
+	
+	// Returns a string indicating what this dog says. You may choose what your 
+            // dog says.
+	public String speak()
+	{
+		return "roof roof";
+
+	}
+	
+}
