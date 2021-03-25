5c8593339aeceb5a973f681ab746834b4afa03c3
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-03-25 13:09:52 -0500

    added Snake.java

---
+++ src/unit9TestInheritance/Snake.java
@@ -0,0 +1,48 @@
+package unit9TestInheritance;
+
+/**
+ * Represents a snake, which is a pet.
+ */
+public class Snake extends Pet{	
+   // Write a constructor that creates a snake with the given name, species, length, and color.
+	public Snake(String n, String s) {
+		super(n, "snake");
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	// Write a method that returns this snake's length
+	
+
+
+
+	
+
+	// Write a method that returns this snake's color
+	
+
+
+
+	
+
+	// Returns a string representation of this snake. The string should have the 
+	// following format: <snakes’ name> "  is a " <species> " that is  " <color>  " and "  				          <length> " inches long"
+
+
+	public String toString() {
+		return "something";
+	}
+
+	
+
+
+	//Returns a string indicating what this snake says
+	
+	
+
+
+
+}
