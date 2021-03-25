aed3b53b4376b5defbcbf5adec5655767d36f1ab
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-03-25 11:21:59 -0500

    PetApp.java tests Dog

--- src/unit9TestInheritance/PetApp.java
+++ src/unit9TestInheritance/PetApp.java
@@ -4,6 +4,7 @@ public class PetApp {
 	
 	public static void main(String[] args) {
 		
+		//Testing Pet
 		Pet pet = new Pet("Abe", "ardvark");
 		
 		String theSpecies = pet.getSpecies();
@@ -13,8 +14,18 @@ public class PetApp {
 		
 		System.out.println("and the toString way:  " + pet.toString());
 		
+		//Testing Dog
+		Pet dog = new Dog("Charlie", "dog", "Husky");
+		// for testing case where reference Type is run type
+		Dog dog2 = new Dog("Doug", "dog", "Husky");
 		
-	}
-	
+		String theSpeciesDog = dog.getSpecies();
+		String theNameDog = dog.getName();
 
+		System.out.println("pet the manual way:  " + theSpeciesDog + ", " + theNameDog);
+		System.out.println("It seems that dog object can't give a breed because its Reference/Declared type is Pet which has no getBreed method");
+		System.out.println("   However, downcasting makes it work:  String theBreed = ((Dog) dog).getBreed()");
+		
+		System.out.println("Info on dog2 (declared type and run type both Dog) using toString:  " + dog2.toString());	
+	}
 }
